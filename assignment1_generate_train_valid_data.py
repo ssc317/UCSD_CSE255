498e10313a70b15380fb21fcd9ab11b77b2a1c91
ssc317 <ssc317fp@gmail.com>
2015-11-10 22:08:48 -0800

    print

---
+++ assignment1_generate_train_valid_data.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 10 22:04:42 2015
+
+@author: ssc317
+"""
+import gzip
+from collections import defaultdict
+import mylib
+def readGz(f):
+  for l in gzip.open(f):
+    yield eval(l)
+
+data_ = []
+for l in readGz("../train.json.gz"):
+    data_append(l)
+train_data = data[:500000]
+valid_data = data[500000:]
+mylib.saveData('../train_valid_1M',[train_data, valid_data])
+mylib.saveData('../train_1M',[data_])
---
+++ assignment1_mode0_1M.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 10 22:02:00 2015
+
+@author: ssc317
+"""
+
