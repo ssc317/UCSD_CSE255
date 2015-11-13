22f29de78d8aa52167cc23282d92b639740f33fa
ssc317 <ssc317fp@gmail.com>
2015-11-13 00:11:34 -0800

    a

--- assignment1_rating.py
+++ assignment1_rating.py
@@ -36,40 +36,54 @@ beta_i0 = { iid: 0 for iid in Ui.keys()}
 rating_valid_y = [d['rating'] for d in valid_data]
 def mse(rating_valid_predict, rating_valid_y):
         return sum((a-b)**2 for a,b in zip(rating_valid_predict,rating_valid_y)) / len(rating_valid_y)
-lamda = 4
-iterNum = 0
-beta_u = beta_u0
-beta_i = beta_i0
-alpha = alpha0
-while iterNum > -1:
-    new_alpha = sum([Rui[(t['reviewerID'], t['itemID'])] - beta_u[t['reviewerID']] - beta_i[t['itemID']] for t in train_data]) / len(train_data)
-    new_beta_i, new_beta_u = defaultdict(int), defaultdict(int)
-    for u in Iu.keys():
-        new_beta_u[u] = sum([ Rui[(u,i)] - new_alpha - beta_i[i] for i in Iu[u]]) / (lamda + len(Iu[u]))
-    for i in Ui.keys():
-        new_beta_i[i] = sum([ Rui[(u,i)] - alpha - new_beta_u[u] for u in Ui[i]]) / (lamda + len(Ui[i]))
-    covariance_alpha = abs(alpha - new_alpha)**2
-    covariance_beta_i, covariance_beta_u = 0,0
-    for i in beta_i:
-        covariance_beta_i += abs(beta_i[i] - new_beta_i[i])**2
-    for u in beta_u:
-        covariance_beta_u += abs(beta_u[u] - new_beta_u[u])**2
-    covariance = numpy.sqrt(covariance_alpha + covariance_beta_i + covariance_beta_u)
-    covariance = [(alpha - new_alpha)**2] + [(b-nb)**2 for b,nb in zip(beta_i.values(), new_beta_i.values())] + [(b-nb)**2 for b,nb in zip(beta_u.values(), new_beta_u.values())]
-    covariance = sum(numpy.sqrt(covariance))
-    print "covariance is "+str(covariance)
-    #if(alpha==new_alpha and beta_i == new_beta_i and beta_u == new_beta_u):
-    if(covariance < 1e-10):
-        break
-    else:
-        alpha, beta_i, beta_u, iterNum = new_alpha, new_beta_i, new_beta_u, iterNum+1
-        print "Finish iter " + str(iterNum) + "with lamda " + str(lamda)
+lamdas = [1,2,3,4,5,6,7,8,9,10]
+thetas = []
+MSEs = []
+for lamda in lamdas:
+    iterNum = 0
+    beta_u = beta_u0
+    beta_i = beta_i0
+    alpha = alpha0
+    while iterNum > -1:
+        new_alpha = sum([Rui[(t['reviewerID'], t['itemID'])] - beta_u[t['reviewerID']] - beta_i[t['itemID']] for t in train_data]) / len(train_data)
+        new_beta_i, new_beta_u = defaultdict(int), defaultdict(int)
+        for u in Iu.keys():
+            new_beta_u[u] = sum([ Rui[(u,i)] - new_alpha - beta_i[i] for i in Iu[u]]) / (lamda + len(Iu[u]))
+        for i in Ui.keys():
+            new_beta_i[i] = sum([ Rui[(u,i)] - alpha - new_beta_u[u] for u in Ui[i]]) / (lamda + len(Ui[i]))
+        covariance_alpha = abs(alpha - new_alpha)**2
+        covariance_beta_i, covariance_beta_u = 0,0
+        for i in beta_i:
+            covariance_beta_i += abs(beta_i[i] - new_beta_i[i])**2
+        for u in beta_u:
+            covariance_beta_u += abs(beta_u[u] - new_beta_u[u])**2
+        covariance = numpy.sqrt(covariance_alpha + covariance_beta_i + covariance_beta_u)
+        covariance = [(alpha - new_alpha)**2] + [(b-nb)**2 for b,nb in zip(beta_i.values(), new_beta_i.values())] + [(b-nb)**2 for b,nb in zip(beta_u.values(), new_beta_u.values())]
+        covariance = sum(numpy.sqrt(covariance))
+        print "covariance is "+str(covariance)
+        #if(alpha==new_alpha and beta_i == new_beta_i and beta_u == new_beta_u):
+        if(covariance < 1e-10):
+            break
+        else:
+            alpha, beta_i, beta_u, iterNum = new_alpha, new_beta_i, new_beta_u, iterNum+1
+            print "Finish iter " + str(iterNum) + "with lamda " + str(lamda)
+        rating_valid_parameters = []
+        for d in valid_data:
+            rating_valid_parameters.append([alpha, beta_u[d['reviewerID']], beta_i[d['itemID']]])
+        rating_valid_predict = [sum(para) for para in rating_valid_parameters]
+        rating_valid_MSE = mse(rating_valid_predict, rating_valid_y)
+        MSEs.append(rating_valid_MSE)
+        thetas.append([alpha,beta_u,beta_i])
+        print "MSE of validation set is "+ str(rating_valid_MSE)
+print MSEs
 # In[] validation
+index = thetas.index(min(MSEs))
+[alpha,beta_u,beta_i] = thetas[index]
 rating_valid_parameters = []
 for d in valid_data:
     rating_valid_parameters.append([alpha, beta_u[d['reviewerID']], beta_i[d['itemID']]])
 
 rating_valid_predict = [sum(para) for para in rating_valid_parameters]
 rating_valid_MSE = mse(rating_valid_predict, rating_valid_y)
-print "MSE of validation set is "+ str(rating_valid_MSE)
+print "MSE of validation set is "+ str(rating_valid_MSE) + " with lamda "+ str(index+1)
 mylib.saveData('1M_train_rating',[alpha, beta_u, beta_i])
